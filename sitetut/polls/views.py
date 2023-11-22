3c51a1898ec6e37f7a8f656868b549e89c23fc95
rostik <rostik127qw@gamil.com>
2023-11-22 18:04:18 +0300

    добавил тесты

--- sitetut/polls/views.py
+++ sitetut/polls/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-
 from .models import Choice, Question
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -80,3 +80,22 @@ def vote(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/results.html", {"question": question})
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    @property
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+    def get_queryset(self):
+        """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+               :5
+            ]
