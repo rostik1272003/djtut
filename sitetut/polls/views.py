12d0f3d39024631489acb668d6f47274c6e319f7
rostik <rostik127qw@gamil.com>
2023-12-04 17:00:57 +0300

    добавил фон поправлены индексы правки в стилях добавлена модель question

--- sitetut/polls/views.py
+++ sitetut/polls/views.py
@@ -103,7 +103,8 @@ class IndexView(generic.ListView):
 
 
 class DetailView(generic.DetailView):
-    ...
+    model = Question
+    template_name = "polls/detail.html"
 
     def get_queryset(self):
         """
